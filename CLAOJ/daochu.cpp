5a3aa5da9eb921841c75741fc599b292dde02f11
lethanhdat <lds050606@gmail.com>
2023-04-09 23:40:13 +0700

    9/4

---
+++ CLAOJ/daochu.cpp
@@ -0,0 +1,71 @@
+// Template //
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for(ll i=a, _b=b; i<=_b; i++)
+#define FORD(i, a, b) for(ll i=a, _b=b; i>=_b; i--)
+#define pb push_back
+#define ALL(a) a.begin(), a.end()
+#define mp make_pair
+#define fi first
+#define se second
+#define mset(a, b) memset(a, b, sizeof(a))
+#define MASK(i) (1LL<<(i))
+#define BIT(x, i) (((x)>>(i))&1)
+#define task "SUADUONG"
+
+using namespace std;
+typedef int64_t ll;
+typedef long double ld;
+typedef pair<ll, ll> ii;
+typedef pair<ll, ii> iii;
+const ll maxn=2*1e4+2;
+const ll mod=26051968;
+const ll inf=1e18;
+
+const int moveX[]={0,0,1,-1};
+const int moveY[]={-1,1,0,0};
+
+bool maximize(ll &A, ll B)
+{
+    return A<B ? A=B, true : false;
+}
+
+bool minimize(ll &A, ll B)
+{
+    return A>B ? A=B, true : false;
+}
+
+ll POW(ll a, ll b)
+{
+    if (b==0) return 1;
+    ll tmp=POW(a, b/2);
+    return b%2==0 ? (tmp*tmp)%mod : (tmp*tmp*a) % mod;
+}
+
+//main
+
+string s1,s2;
+
+void input()
+{
+	cin>>s1>>s2;
+}
+
+void lds_go_goooo()
+{
+	cout<<s2<<' '<<s1<<endl;
+}
+
+int main()
+{
+ 	ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    //freopen(task".INP", "r", stdin);
+    //freopen(task".OUT", "w", stdout);
+    ll test_case=1; cin>>test_case;
+    while(test_case--)
+    {
+        input(), lds_go_goooo();
+        cout<<'\n';
+    }
+    return 0;
+}
---
+++ CLAOJ/wrongdigit.cpp
@@ -0,0 +1,111 @@
+// Template //
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for(ll i=a, _b=b; i<=_b; i++)
+#define FORD(i, a, b) for(ll i=a, _b=b; i>=_b; i--)
+#define pb push_back
+#define ALL(a) a.begin(), a.end()
+#define mp make_pair
+#define fi first
+#define se second
+#define mset(a, b) memset(a, b, sizeof(a))
+#define MASK(i) (1LL<<(i))
+#define BIT(x, i) (((x)>>(i))&1)
+#define task "SUADUONG"
+
+using namespace std;
+typedef int64_t ll;
+typedef long double ld;
+typedef pair<ll, ll> ii;
+typedef pair<ll, ii> iii;
+const ll maxn=2*1e4+2;
+const ll mod=26051968;
+const ll inf=1e18;
+
+const int moveX[]={0,0,1,-1};
+const int moveY[]={-1,1,0,0};
+
+bool maximize(ll &A, ll B)
+{
+    return A<B ? A=B, true : false;
+}
+
+bool minimize(ll &A, ll B)
+{
+    return A>B ? A=B, true : false;
+}
+
+ll POW(ll a, ll b)
+{
+    if (b==0) return 1;
+    ll tmp=POW(a, b/2);
+    return b%2==0 ? (tmp*tmp)%mod : (tmp*tmp*a) % mod;
+}
+
+//main
+
+string s1,s2;
+
+string findSum(string str1, string str2)
+{
+    
+    if (str1.length() > str2.length())
+        swap(str1, str2);
+    string str = "";
+    int n1 = str1.length(), n2 = str2.length();
+    reverse(str1.begin(), str1.end());
+    reverse(str2.begin(), str2.end());
+ 
+    int carry = 0;
+    for (int i=0; i<n1; i++)
+    {
+        int sum = ((str1[i]-'0')+(str2[i]-'0')+carry);
+        str.push_back(sum%10 + '0');
+        carry = sum/10;
+    }
+
+    for (int i=n1; i<n2; i++)
+    {
+        int sum = ((str2[i]-'0')+carry);
+        str.push_back(sum%10 + '0');
+        carry = sum/10;
+    }
+    if (carry)
+        str.push_back(carry+'0');
+    reverse(str.begin(), str.end());
+ 
+    return str;
+}
+
+
+void input()
+{
+	cin>>s1>>s2;
+}
+
+string change(string str,char a, char b)
+{
+	for(int i=0;i<str.size();i++)
+		if(str[i]==a)
+			str[i]=b;
+	return str;
+}
+
+void lds_go_goooo()
+{
+	cout<<findSum(change(s1,'6','5'),change(s2,'6','5'))<<" "<<findSum(change(s1,'5','6'),change(s2,'5','6'));
+}
+
+int main()
+{
+ 	ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    //freopen(task".INP", "r", stdin);
+    //freopen(task".OUT", "w", stdout);
+    ll test_case=1; //cin>>test_case;
+    while(test_case--)
+    {
+        input(), lds_go_goooo();
+        cout<<'\n';
+    }
+    return 0;
+}
